dc2ebdc8a039700c71b68824068235819a5895a9
제호 이 <jhl81094@gmail.com>
2021-08-14 22:13:58 +0900

    bug update

--- testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/settings/PrivateFix.kt
+++ testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/settings/PrivateFix.kt
@@ -2,11 +2,27 @@ package org.techtown.testrecyclerview.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import org.techtown.testrecyclerview.R
 
 class PrivateFix : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_private_fix)
+
+        supportActionBar?.setTitle("내 정보 수정하기")
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.setting_detail, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle presses on the action bar items
+        when(item.itemId){
+            R.id.fixActionBtn -> { finish() }}
+        return super.onOptionsItemSelected(item)
     }
 }
--- testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/tutorial/Sex.kt
+++ testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/tutorial/Sex.kt
@@ -25,12 +25,11 @@ class Sex : AppCompatActivity() {
         val intentBtn = findViewById<Button>(R.id.intentBtn)
         intentBtn.text = "다음"
 
-        var result : String
         var clicked : Boolean = false
         var idCheck : AppCompatButton?= null
 
         manBtn.setOnClickListener {
-            if (idCheck == null || clicked == false){ // 아무 것도 클릭 안되어 있음
+            if (idCheck == null && clicked == false){ // 아무 것도 클릭 안되어 있음
                 idCheck = manBtn
                 clicked = true
                 manBtn.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.btn_background))
@@ -51,7 +50,7 @@ class Sex : AppCompatActivity() {
         }
 
         womanBtn.setOnClickListener {
-            if (idCheck == null || clicked == false){ // 아무 것도 클릭 안되어 있음
+            if (idCheck == null && clicked == false){ // 아무 것도 클릭 안되어 있음
                 idCheck = womanBtn
                 clicked = true
                 manBtn.setBackgroundDrawable(ContextCompat.getDrawable(this,R.drawable.btn_background))
