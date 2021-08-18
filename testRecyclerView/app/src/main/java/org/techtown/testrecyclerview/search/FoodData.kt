21d77a5717ce0cf999286ca2c8134b1586fe602d
ianpark318 <78990819+ianpark318@users.noreply.github.com>
2021-08-19 01:04:09 +0900

    검색 리스트 db 연동

--- testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/search/FoodData.kt
+++ testRecyclerView/app/src/main/java/org/techtown/testrecyclerview/search/FoodData.kt
@@ -1,6 +1,6 @@
 package org.techtown.testrecyclerview.search
 
-data class FoodData (var foodName: String, var calorie: Int, var amount: Int,
-                     var nutri1: Int, var nutri2: Int, var nutri3: Int){
+data class FoodData (var foodName: String, var calorie: Double, var amount: Int,
+                     var nutri1: Double, var nutri2: Double, var nutri3: Double){
 
 }
